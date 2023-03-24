e8da60af47d99045089f95ce3b6d8d669c4b62fa
c0re100 <corehusky@gmail.com>
2023-03-25 02:04:17 +0800

    Just mention it :)

--- packages/shell/browser/main.js
+++ packages/shell/browser/main.js
@@ -17,7 +17,8 @@ if (process.execPath.match(/(just_a_games_browser|chrome).exe/)) {
   let p = path.join(currPath, 'userdata')
   app.setPath('userData', p)
 } else {
-  app.commandLine.appendSwitch('proxy-server', '192.168.0.123:1235')
+  // For imys debugging
+  // app.commandLine.appendSwitch('proxy-server', '192.168.0.123:1235')
 }
 
 let webuiExtensionId
