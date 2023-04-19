2dbead2054481bf671f7ef0425d5405fe4b54ad6
c0re100 <corehusky@gmail.com>
2023-04-20 06:50:38 +0800

    Update afterPack script

--- packages/shell/script/after.js
+++ packages/shell/script/after.js
@@ -9,15 +9,24 @@ exports.default = async function (context) {
     } else if (env === 'linux') {
         unpack_dir = 'linux-unpacked'
     } else if (env === 'darwin') {
-        unpack_dir = 'darwin'
+        unpack_dir = 'mac'
     }
 
     if (unpack_dir) {
         console.log("\n- [JAGB] Copy extensions to unpacked build...\n");
-        fs.mkdirSync('./build/' + unpack_dir + '/extensions')
-        fs.cpSync('../../extensions/uBlock', './build/' + unpack_dir + '/extensions/uBlock', {recursive: true})
-        fs.cpSync('../../extensions/stylus', './build/' + unpack_dir + '/extensions/stylus', {recursive: true})
-        fs.cpSync('../../extensions/Violentmonkey', './build/' + unpack_dir + '/extensions/Violentmonkey', {recursive: true})
+
+        if (env === 'darwin') {
+            fs.mkdirSync('./build/' + unpack_dir + '/just_a_games_browser.app/Contents/extensions')
+            fs.cpSync('../../extensions/uBlock', './build/' + unpack_dir + '/just_a_games_browser.app/Contents/extensions/uBlock', {recursive: true})
+            fs.cpSync('../../extensions/stylus', './build/' + unpack_dir + '/just_a_games_browser.app/Contents/extensions/stylus', {recursive: true})
+            fs.cpSync('../../extensions/Violentmonkey', './build/' + unpack_dir + '/just_a_games_browser.app/Contents/extensions/Violentmonkey', {recursive: true})
+        } else {
+            fs.mkdirSync('./build/' + unpack_dir + '/extensions')
+            fs.cpSync('../../extensions/uBlock', './build/' + unpack_dir + '/extensions/uBlock', {recursive: true})
+            fs.cpSync('../../extensions/stylus', './build/' + unpack_dir + '/extensions/stylus', {recursive: true})
+            fs.cpSync('../../extensions/Violentmonkey', './build/' + unpack_dir + '/extensions/Violentmonkey', {recursive: true})
+        }
+
         if (env === 'win32') {
             fs.renameSync('./build/' + unpack_dir + '/just_a_games_browser.exe', './build/' + unpack_dir + '/chrome.exe')
         }
