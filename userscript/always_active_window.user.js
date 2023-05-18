9b3996832c84032d1a0bbe844475029f9d793868
c0re100 <corehusky@gmail.com>
2023-05-19 05:44:55 +0800

    [Userscript] Patch rAF when Cocos2d detected

--- userscript/always_active_window.user.js
+++ userscript/always_active_window.user.js
@@ -2,12 +2,12 @@
 // @name        Always Active Window
 // @namespace   Always Active Window
 // @author      c0re100
-// @description Override hide/visibility property
+// @description Override hide/visibility property, override rAF if game type is Cocos2d
 // @homepage    https://github.com/c0re100/just_a_game_browser
 // @include     *://*
 // @grant       none
-// @version     1.0.0
-// @run-at      document-start
+// @version     1.1
+// @run-at      document-body
 // ==/UserScript==
 
 Object.defineProperty(document, 'hidden', {
@@ -18,3 +18,24 @@ Object.defineProperty(document, 'visibilityState', {
     value: 'visible',
     configurable: false
 });
+
+function rAFPatch() {
+    window.requestAnimationFrame = new Proxy(window.requestAnimationFrame, {
+        apply(target, self, args) {
+            return setTimeout(function () {
+                args[0](performance.now());
+            }, 4);
+        }
+    });
+    window.cancelAnimationFrame = new Proxy(window.cancelAnimationFrame, {
+        apply(target, self, args) {
+            clearTimeout(args[0]);
+            return Reflect.apply(target, self, args);
+        }
+    });
+}
+
+if (document.getElementById("GameCanvas")) {
+    console.log('Cocos2d detected, patching rAF...')
+    rAFPatch();
+}
